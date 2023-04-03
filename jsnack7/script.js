6aa3ff51cea2259f4614ba059948764d1988374f
MillionDollarBob <riccardo.c1995@outlook.com>
2023-04-03 17:12:10 +0200

    7 fatto

--- jsnack7/script.js
+++ jsnack7/script.js
@@ -1,11 +1,27 @@
-let input1 = prompt("Inserisci la prima parola");
-let input2 = prompt("Inserisci la seconda parola");
+/* Scrivere un programma che stampi la tabellina del 2, fino al numero 1000.
 
-if(isNaN(input1) && isNaN(input2)){
+Modificate poi il programma in modo che venga chiesto all'utente il
+numero massimo consentito, e stampare tutta la tabellina del 2 fino al
+numero inserito */
 
-    if (input1.length > input2.length){
-        console.log(input1 + input2);
-    }else {
-        console.log(input2 + input1);
+/* primo codice
+
+let output = 0;
+
+for(i = 1 ; output < 1000 ; i++){
+    output = i * 2;
+    console.log(output); 
+}
+
+*/
+
+let contatore = prompt("A quanto vuoi arrivare");
+let output = 0;
+contatore = parseInt(contatore);
+
+if(!isNaN(contatore) && contatore > 0){
+    for(i = 1 ; output < contatore - 1 ; i++){
+        output = i * 2;
+        console.log(output); 
     }
-}else console.log("gli input non sono validi");
+}
